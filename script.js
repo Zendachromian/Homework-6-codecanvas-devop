82763b42f6e8f56a53ae1de544570a3555c7dc4d
Zendachromian <103535913+Zendachromian@users.noreply.github.com>
2023-12-21 18:34:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,76 +3,63 @@ const screen = document.querySelector(".screen");
 let currentNumber = "";
 let storedNumber = "";
 let operator = "";
-let decimalAdded = false;
-
-
-let calculator = document.getElementById('calculator');
-
-operators.forEach(operator => {
- operator.addEventListener('click', () => {
- calculator.dataset.firstValue = display.value;
- calculator.dataset.operator = operator.innerText;
- display.value = '';
- });
-});
 
 // Function to display a number on the screen
 function displayNumber(number) {
-   if (number === "." && decimalAdded) return;
-   if (number === ".") decimalAdded = true;
-   currentNumber += number;
-   screen.textContent = currentNumber;
+    currentNumber += number;
+    screen.textContent = currentNumber;
 }
 
 // Function to handle operator clicks
-function handleOperator(op) {
-   if (operator !== "") {
-       storedNumber = eval(storedNumber + operator + currentNumber);
-       currentNumber = "";
-   } else {
-       storedNumber = currentNumber;
-       currentNumber = "";
-   }
-   operator = op;
-   decimalAdded = false;
+function handleOperator(operator) {
+    storedNumber = currentNumber;
+    currentNumber = "";
+    this.operator = operator;
 }
 
 // Function to handle clear button click
 function handleClear() {
-   currentNumber = "";
-   storedNumber = "";
-   operator = "";
-   decimalAdded = false;
-   screen.textContent = "0";
+    currentNumber = "";
+    storedNumber = "";
+    operator = "";
+    screen.textContent = "0";
 }
 
 // Function to handle equal button click
 function handleEqual() {
-   if (operator === "") return;
-   if (currentNumber === "") {
-       screen.textContent = storedNumber;
-   } else {
-       let result = eval(storedNumber + operator + currentNumber);
-       screen.textContent = result;
-       currentNumber = result.toString();
-   }
-   operator = "";
-   decimalAdded = false;
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = parseFloat(storedNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(storedNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(storedNumber) * parseFloat(currentNumber);
+            break;
+        case "/":
+            result = parseFloat(storedNumber) / parseFloat(currentNumber);
+            break;
+    }
+    screen.textContent = result;
+    currentNumber = result.toString();
+    operator = "";
 }
 
 // Add event listeners to buttons
 const numberButtons = document.querySelectorAll(".number-button");
 numberButtons.forEach(button => {
-   button.addEventListener("click", () => {
-       displayNumber(button.textContent);
-   });
+    button.addEventListener("click", () => {
+        displayNumber(button.textContent);
+    });
 });
 
 const operatorButtons = document.querySelectorAll(".operator-button");
 operatorButtons.forEach(button => {
-   button.addEventListener("click", () => {
-       handleOperator(button.textContent);
-   });
+    button.addEventListener("click", () => {
+        handleOperator(button.textContent);
+    });
 });
 
 const clearButton = document.querySelector(".clear-button");
@@ -80,9 +67,3 @@ clearButton.addEventListener("click", handleClear);
 
 const equalButton = document.querySelector(".equal-button");
 equalButton.addEventListener("click", handleEqual);
-
-const decimalButton = document.querySelector(".decimal-button");
-decimalButton.addEventListener("click", () => {
-   displayNumber(".");
-});
-
