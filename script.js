cf2bfd0c3fac388ada6647fe53e6ac49ca1dd78d
Zendachromian <103535913+Zendachromian@users.noreply.github.com>
2023-12-21 18:03:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,76 @@
-let display = document.getElementById('display');
-let digits = document.querySelectorAll('.digit');
-let operators = document.querySelectorAll('.operator');
-let clear = document.getElementById('clear');
-let equals = document.getElementById('equals');
+// Select the screen element
+const screen = document.querySelector(".screen");
+let currentNumber = "";
+let storedNumber = "";
+let operator = "";
+let decimalAdded = false;
 
-digits.forEach(digit => {
- digit.addEventListener('click', () => {
-   display.value += digit.innerText;
- });
+// Function to display a number on the screen
+function displayNumber(number) {
+   if (number === "." && decimalAdded) return;
+   if (number === ".") decimalAdded = true;
+   currentNumber += number;
+   screen.textContent = currentNumber;
+}
+
+// Function to handle operator clicks
+function handleOperator(op) {
+   if (operator !== "") {
+       storedNumber = eval(storedNumber + operator + currentNumber);
+       currentNumber = "";
+   } else {
+       storedNumber = currentNumber;
+       currentNumber = "";
+   }
+   operator = op;
+   decimalAdded = false;
+}
+
+// Function to handle clear button click
+function handleClear() {
+   currentNumber = "";
+   storedNumber = "";
+   operator = "";
+   decimalAdded = false;
+   screen.textContent = "0";
+}
+
+// Function to handle equal button click
+function handleEqual() {
+   if (operator === "") return;
+   if (currentNumber === "") {
+       screen.textContent = storedNumber;
+   } else {
+       let result = eval(storedNumber + operator + currentNumber);
+       screen.textContent = result;
+       currentNumber = result.toString();
+   }
+   operator = "";
+   decimalAdded = false;
+}
+
+// Add event listeners to buttons
+const numberButtons = document.querySelectorAll(".number-button");
+numberButtons.forEach(button => {
+   button.addEventListener("click", () => {
+       displayNumber(button.textContent);
+   });
 });
 
-operators.forEach(operator => {
- operator.addEventListener('click', () => {
-   display.value += operator.innerText;
- });
+const operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach(button => {
+   button.addEventListener("click", () => {
+       handleOperator(button.textContent);
+   });
 });
 
-clear.addEventListener('click', () => {
- display.value = '';
-});
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", handleClear);
 
-equals.addEventListener('click', () => {
- try {
-   display.value = eval(display.value);
- } catch {
-   display.value = 'Error';
- }
+const equalButton = document.querySelector(".equal-button");
+equalButton.addEventListener("click", handleEqual);
+
+const decimalButton = document.querySelector(".decimal-button");
+decimalButton.addEventListener("click", () => {
+   displayNumber(".");
 });
